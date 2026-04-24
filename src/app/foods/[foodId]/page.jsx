da6f8aa52be74cbc7c66fdf496f459cea6e8b9d0
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-24 21:08:12 +0600

    added category to the single food item

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -5,11 +5,12 @@ const page = async({params}) => {
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
     const data = await res.json();
     const food = data.data;
-    const {id, dish_name, image_link} = food;
+    const {id, dish_name, image_link, category} = food;
     return (
         <div>
             <h1>Dish Id: {id}</h1>
             <h2>The food is: {dish_name}</h2>
+            <p>Category: {category}</p>
             <Image src={image_link} alt={dish_name} width={200} height={100} />
         </div>
     );
