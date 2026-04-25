913d740a369533d874bc61d2ff53b5b3b6258059
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-25 21:09:03 +0600

    added some designs to the single card foods

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -8,9 +8,10 @@ const page = async({params}) => {
     const {id, dish_name, image_link, category, rating, price} = food;
     return (
         <div className="flex justify-center items-center">
-            <div className="border border-gray-200 shadow-2xl rounded-2xl p-5 space-y-5 text-center max-w-100">
+            <div className="border border-gray-200 shadow-2xl rounded-2xl px-5 py-10 space-y-5 text-center max-w-100">
             <h1 className="text-4xl font-bold">Dish Id: {id}</h1>
             <h2 className="text-xl font-semibold">The food is: {dish_name}</h2>
+            <hr />
             <p className="text-lg mb-5">Category: {category}</p>
             <Image className="mx-auto" src={image_link} alt={dish_name} width={200} height={100} />
             <div className="flex justify-between items-center font-semibold">
