03153e874c1e967ef608008f47db0df716c94b3a
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-25 21:05:57 +0600

    added some new features to the single card

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -5,7 +5,7 @@ const page = async({params}) => {
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
     const data = await res.json();
     const food = data.data;
-    const {id, dish_name, image_link, category} = food;
+    const {id, dish_name, image_link, category, rating, price} = food;
     return (
         <div className="flex justify-center items-center">
             <div className="border border-gray-200 shadow-2xl rounded-2xl p-5 space-y-5 text-center max-w-100">
@@ -13,6 +13,10 @@ const page = async({params}) => {
             <h2 className="text-xl font-semibold">The food is: {dish_name}</h2>
             <p className="text-lg mb-5">Category: {category}</p>
             <Image className="mx-auto" src={image_link} alt={dish_name} width={200} height={100} />
+            <div className="flex justify-between items-center font-semibold">
+                <p>Rating: {rating}</p>
+                <p>Price: {price}</p>
+            </div>
         </div>
         </div>
     );
